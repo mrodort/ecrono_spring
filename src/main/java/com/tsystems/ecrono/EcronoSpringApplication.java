87de46cccc0a8135522aa7972fd60ac7c2fcc77b
PC48\userPC49 <maysunderground@gmail.com>
2017-01-16 15:40:48 +0100

    initial

---
+++ src/main/java/com/tsystems/ecrono/EcronoSpringApplication.java
@@ -0,0 +1,12 @@
+package com.tsystems.ecrono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcronoSpringApplication {
+
+    public static void main(String[] args) {
+	SpringApplication.run(EcronoSpringApplication.class, args);
+    }
+}
